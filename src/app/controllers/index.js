253822198c3667784561806b6193526242b8f189
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-25 03:27:56 +0000

    add user manage feature

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,7 +1,9 @@
 const homeController = require('./home');
 const authController = require('./auth');
+const userController = require('./user');
 
 module.exports = {
   homeController,
   authController,
+  userController,
 };
