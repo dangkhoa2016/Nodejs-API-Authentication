8f4245145d07a6190beba06e6f68b7c795cf9a28
dangkhoa2016 <i.am@dangkhoa.dev>
2025-01-26 08:54:43 +0000

    refactor database migrate

--- manual/node.js
+++ manual/node.js
@@ -9,6 +9,7 @@ var now = Date.now() / 1e3 | 0;
   require('dotenv').config();
 
   var models = require('./src/app/models');
+  // await models.User.sync({ force: false });
   await models.sequelize.sync({ force: false });
 
   var user = await models.User.create({ email: 'test@user.local', username: 'testuser', });
@@ -17,5 +18,7 @@ var now = Date.now() / 1e3 | 0;
 
   var users = await models.User.unscoped().findAll();
   var user = await models.User.findOne();
+  var user = await models.User.findOne({ order: [['id', 'DESC']] });
+  var count = await models.User.count();
 
 })();
